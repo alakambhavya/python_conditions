1dd4ec3db7c39d4bbe968969fca10ae11d735303
alakambhavya <alakambhavya@gmail.com>
2026-06-01 09:19:19 +0530

    if statements

---
+++ if_stmt.py
@@ -0,0 +1,74 @@
+
+print("\n1.check a number is positive.\n")
+num=487
+if num>0:
+	print("num is a positive number")
+
+print("____________________")
+
+print("\n2.check a number is negetive.\n")
+num=-27
+if num<0:
+	print("num is a negetive number")
+
+print("____________________")
+
+
+print("\n3.check a number is even.\n")
+num=48
+if num%2==0:
+	print("num is a even number")
+
+print("_______________________")
+
+print("\n4.check a number is odd.\n")
+num=49
+if num%2!=0:
+	print("num is a odd number")
+
+
+print("______________________")
+
+print("\n5.Check whether a person is eligible to vote.\n")
+age=50
+if age>=18:
+	print("eligible to vote")
+
+print("________________")
+
+print("\n6.Check whether a number is divisible by 5.\n")
+num=125
+if num%5==0:
+	print("num is divisible by 5")
+
+print("________________")
+
+print("\n7.Check whether a number is prime.\n")
+num=17
+if  num>1 and num%2!=0:
+	print("num is prime")
+
+print("____________________")
+
+print("\n8.Check whether a number is odd.\n")
+num=23
+if num%2!=0:
+	print("num is odd")
+
+print("________________")
+
+print("\n9.Check whether a number is divisible by both 3 and 7.\n")
+num=21
+if num%3==0 and num%7==0:
+	print("dicisible by 3 and 7")
+
+print("________________________")
+
+print("\n10.find the maximum of two numbers.\n")
+a=10
+b=20
+if a>b:
+	print("maximum num is:",a)
+if a<b:
+	print("maximum num is:",b)
+
